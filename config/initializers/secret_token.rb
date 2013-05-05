8b695364aa1834852ca87ec740a1871fffbbdfa0
jsdolber <sdolber@gmail.com>
2013-05-05 19:51:04 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Savethereader::Application.config.secret_token = '1c639f506c55638d8b27629682fb696148e831502512c18cb40fc07a7fed9b95f9d8d24a608e41bf35520c9ce0476bc3cda3bcecba3b3ad546f784dd8c4cb96f'
