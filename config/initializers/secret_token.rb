49fb59ced0bc4e2f692436f4212fd6853afe5090
jsdolber <sdolber@gmail.com>
2013-05-05 22:08:14 -0400

    added feed model

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Savethereader::Application.config.secret_token = '1c639f506c55638d8b27629682fb696148e831502512c18cb40fc07a7fed9b95f9d8d24a608e41bf35520c9ce0476bc3cda3bcecba3b3ad546f784dd8c4cb96f'
+Savethereader::Application.config.secret_token = '1c639f506c55638d8629682fb696148e831502512c18cb40fc07a7fed9bf9d8a608e41bf35520476bc3cda3bcecba3b3ad546f784dd8c4cb96f'
